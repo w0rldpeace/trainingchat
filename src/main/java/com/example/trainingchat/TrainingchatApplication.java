1c0d49adffeb849a133bf39840ee21c0afc3c09b
w0rldpeace <rk.home42@gmail.com>
2022-09-20 10:23:49 +0200

    Initial Commit

---
+++ src/main/java/com/example/trainingchat/TrainingchatApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainingchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trainingchat/TrainingchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainingchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
