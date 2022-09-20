4bf024835a7715b5ca040f9b0c253115a15cb1d2
w0rldpeace <rk.home42@gmail.com>
2022-09-20 11:12:05 +0200

    Initial Commit, Base Project

---
+++ src/main/java/com/example/trainingchat/controller/ChatController.java
@@ -0,0 +1,37 @@
+package com.example.trainingchat.controller;
+
+import com.example.trainingchat.model.ChatMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class ChatController {
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat.sendMessage/{id}/")
+    @SendTo("/topic/public/{id}") // localhost:8080/topic/public
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage, @PathVariable Long id) {
+        // ChatRoom chatroom = chatRoomRepository.findById();
+
+        messagingTemplate.convertAndSend("/topic/public/{id}", chatMessage);
+
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
